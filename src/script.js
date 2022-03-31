b6c3602ad7676e148beffede25d1623e4bc723ce
Filip Malm-Bägén <Filip.bagen@gmail.com>
2022-03-31 14:00:31 +0200

    Updated background

--- src/script.js
+++ src/script.js
@@ -1,51 +1,38 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import * as dat from 'dat.gui'
 
 const gltfloader = new GLTFLoader()
-
-// Debug
-const gui = new dat.GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-
-console.log('test1')
+// scene.background = new THREE.Color('cornflowerblue')
 
 //earth
-gltfloader.load('earthpolycartest.glb', (gltf) => {
-  scene.add(gltf.scene)
-  gui.add(gltf.scene.rotation, 'y').min(0).max(9)
+gltfloader.load('earthpolycartest.glb', (earth) => {
+  let object = earth.scene
+  object.position.set(0,0,0)
+  scene.add(object)
 })
 
-// // Objects
-// const geometry = new THREE.TorusGeometry(0.7, 0.2, 16, 100)
-
-// // Materials
-
-// const material = new THREE.MeshBasicMaterial()
-// material.color = new THREE.Color(0xff0000)
-
-// // Mesh
-// const sphere = new THREE.Mesh(geometry, material)
-// scene.add(sphere)
+//cloud
+// gltfloader.load('cloud.glb', (cloud) => {
+//   let obj = cloud.scene
+//   obj.position.set(0,0,0)
+//   scene.add(obj)
+// })
 
 // Lights
-
 const pointLight = new THREE.PointLight(0xffffff, 5)
 pointLight.position.x = 2
 pointLight.position.y = 3
 pointLight.position.z = 40
 scene.add(pointLight)
 
-/**
- * Sizes
- */
+// Sizes
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
@@ -65,9 +52,7 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-/**
- * Camera
- */
+// Camera
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 0
@@ -75,26 +60,16 @@ camera.position.y = 0
 camera.position.z = 30
 scene.add(camera)
 
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-console.log('test2')
-
-/**
- * Renderer
- */
+// Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
-  alpha: true,
+  alpha: true
 })
+
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
-
+// Animate
 const clock = new THREE.Clock()
 
 const tick = () => {
@@ -105,8 +80,6 @@ const tick = () => {
 
   // Update Orbital Controls
   // controls.update()
-  
-  console.log('test3')
 
   // Render
   renderer.render(scene, camera)
