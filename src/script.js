c15490f171afb2a17939358326c1cec554b09af5
hamreliusfilip <hamreliusfilip@gmail.com>
2022-04-02 22:05:05 +0200

    Rotation, Orbits, Modeller
    Ingen trädstruktur och kaos, men det fungerar.

--- src/script.js
+++ src/script.js
@@ -2,33 +2,71 @@ import './style.css'
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
-const gltfloader = new GLTFLoader()
+var gltfloader = new GLTFLoader()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-// scene.background = new THREE.Color('cornflowerblue')
+//scene.background = new THREE.Color('#00003B')
+
+// Katastrof ordning på koden, men rotationer och orbits fungerar. 
 
 //earth
-gltfloader.load('earthpolycartest.glb', (earth) => {
-  let object = earth.scene
-  object.position.set(0,0,0)
-  scene.add(object)
+let globalEarth; // Global så tick hittar skiten
+gltfloader.load('Earth.glb', (earth) => {
+ let object = earth.scene
+ globalEarth = object
+ object.position.set(0,-5,-10)
+ object.rotation.set(8,4,1)
+ scene.add(object)
+})
+
+//moon
+let globalMoon; 
+gltfloader.load('Moon2.glb', (moon) => {
+ let object = moon.scene
+ globalMoon = object
+   object.position.set(4,16,-10)
+   object.rotation.set(10,8,10)
+ scene.add(object)
 })
 
 //cloud
-// gltfloader.load('cloud.glb', (cloud) => {
-//   let obj = cloud.scene
-//   obj.position.set(0,0,0)
-//   scene.add(obj)
-// })
+let globalCloud;
+gltfloader.load('cloud.glb', (cloud) => {
+   let object = cloud.scene
+   globalCloud = object
+   object.position.set(4,16,-10)
+   object.rotation.set(10,8,10)
+   scene.add(object)
+ })
+
+//cloud 2
+let globalCloud2;
+gltfloader.load('cloud.glb', (cloud2) => {
+   let object = cloud2.scene
+   globalCloud2 = object
+   object.position.set(-15,12,-10)
+   object.rotation.set(0,0,3)
+   scene.add(object)
+ })
+
+//sat
+let globalSat;
+gltfloader.load('sat.glb', (sat) => {
+   let object = sat.scene
+   globalSat = object
+   object.position.set(-20,-5,-10)
+   object.rotation.set(0,0,7)
+   scene.add(object)
+ })
 
 // Lights
-const pointLight = new THREE.PointLight(0xffffff, 5)
-pointLight.position.x = 2
-pointLight.position.y = 3
+const pointLight = new THREE.PointLight(0xffffff, 3)
+pointLight.position.x = 20
+pointLight.position.y = 1
 pointLight.position.z = 40
 scene.add(pointLight)
 
@@ -69,23 +107,45 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// Animate
-const clock = new THREE.Clock()
 
 const tick = () => {
-  const elapsedTime = clock.getElapsedTime()
 
-  // Update objects
-  //sphere.rotation.y = 0.5 * elapsedTime
+  var orbitRadius = 16; 
 
-  // Update Orbital Controls
-  // controls.update()
+  var orbit;
+  orbit = Date.now() * 0.0001;
+
+ if(globalEarth && globalSat && globalCloud && globalCloud2 && globalMoon){
+
+  // Earth 
+  globalEarth.rotation.z += 0.001; 
+
+  // Moon
+  globalMoon.rotation.z += 0.001;
+  globalMoon.position.set(Math.sin(orbit*0.9) * orbitRadius, 0 , 0); 
+
+  // Sat
+  globalSat.rotation.x += 0.01; 
+  globalSat.position.set(Math.cos(orbit*1.3) * orbitRadius, Math.sin(orbit*1.3) * orbitRadius , 0);
+
+  // Cloud
+  //globalCloud.rotation.x += 0.01; 
+  globalCloud.rotation.x += 0.01; 
+  globalCloud.position.set(Math.cos(orbit*1.1) * orbitRadius, Math.sin(orbit*1.1) * orbitRadius , 0);
+
+  // Cloud 2
+  //globalCloud2.rotation.x += 0.01; 
+  globalCloud2.rotation.x += 0.01; 
+  globalCloud2.position.set(Math.cos(orbit*1.15) * orbitRadius, Math.sin(orbit*1.15) * orbitRadius , 0);
+
+}
 
   // Render
   renderer.render(scene, camera)
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick)
+
 }
 
 tick()
