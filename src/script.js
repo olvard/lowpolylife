7e947f59eed9692f28471962f68750ea8946976b
Oliver Lundin <79059027+olvard@users.noreply.github.com>
2022-05-23 11:22:05 +0200

    Revert "top ten"
    This reverts commit 604bf930429ee23b1a1688842657e78c63fa96bb.

--- src/script.js
+++ src/script.js
@@ -11,67 +11,68 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 //scene.background = new THREE.Color('#00003B')
 
-// Katastrof ordning på koden, men rotationer och orbits fungerar.
+// Katastrof ordning på koden, men rotationer och orbits fungerar. 
 
 //earth
-let globalEarth // Global så tick hittar skiten
+let globalEarth; // Global så tick hittar skiten
 gltfloader.load('Earth.glb', (earth) => {
-  let object = earth.scene
-  globalEarth = object
-  object.position.set(0, -5, -10)
-  object.rotation.set(8, 4, 1)
-  scene.add(object)
+ let object = earth.scene
+ globalEarth = object
+ object.position.set(0,-5,-10)
+ object.rotation.set(8,4,1)
+ scene.add(object)
 })
 
 //Sun
-let globalSun // Global så tick hittar skiten
-gltfloader.load('sun.glb', (sun) => {
-  let object = sun.scene
-  globalSun = object
-  object.position.set(0, -8, -13)
-  object.rotation.set(8, 4, 1)
-  scene.add(object)
+let globalSun; // Global så tick hittar skiten
+gltfloader.load('Sun.glb', (sun) => {
+ let object = sun.scene
+ globalSun = object
+ object.position.set(0,-8,-13)
+ object.rotation.set(8,4,1)
+ scene.add(object)
 })
 
+
 //moon
-let globalMoon
-gltfloader.load('moon4.glb', (moon) => {
-  let object = moon.scene
-  globalMoon = object
-  object.position.set(4, 16, -10)
-  object.rotation.set(10, 8, 10)
-  scene.add(object)
+let globalMoon; 
+gltfloader.load('Moon4.glb', (moon) => {
+ let object = moon.scene
+ globalMoon = object
+   object.position.set(4,16,-10)
+   object.rotation.set(10,8,10)
+ scene.add(object)
 })
 
 //cloud
-let globalCloud
+let globalCloud;
 gltfloader.load('cloud.glb', (cloud) => {
-  let object = cloud.scene
-  globalCloud = object
-  object.position.set(4, 16, -13)
-  object.rotation.set(0, 0, 0)
-  scene.add(object)
-})
+   let object = cloud.scene
+   globalCloud = object
+   object.position.set(4,16,-13)
+   object.rotation.set(0,0,0)
+   scene.add(object)
+ })
 
 //cloud 2
-let globalCloud2
+let globalCloud2;
 gltfloader.load('cloud.glb', (cloud2) => {
-  let object = cloud2.scene
-  globalCloud2 = object
-  object.position.set(-15, 12, -13)
-  object.rotation.set(3, 6, 3)
-  scene.add(object)
-})
+   let object = cloud2.scene
+   globalCloud2 = object
+   object.position.set(-15,12,-13)
+   object.rotation.set(3,6,3)
+   scene.add(object)
+ })
 
 //sat
-let globalSat
+let globalSat;
 gltfloader.load('sat.glb', (sat) => {
-  let object = sat.scene
-  globalSat = object
-  object.position.set(-20, -5, -12)
-  object.rotation.set(0, 0, 7)
-  scene.add(object)
-})
+   let object = sat.scene
+   globalSat = object
+   object.position.set(-20,-5,-12)
+   object.rotation.set(0,0,7)
+   scene.add(object)
+ })
 
 // Lights
 const pointLight = new THREE.PointLight(0xffffff, 4)
@@ -80,8 +81,8 @@ pointLight.position.y = 100
 pointLight.position.z = 80
 scene.add(pointLight)
 
-const ambientLight = new THREE.AmbientLight(0x404040, 4) // soft white light
-scene.add(ambientLight)
+const ambientLight = new THREE.AmbientLight(0x404040, 4); // soft white light
+scene.add( ambientLight );
 
 // Sizes
 const sizes = {
@@ -114,66 +115,62 @@ scene.add(camera)
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
-  alpha: true,
+  alpha: true
 })
 
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
+
 const tick = () => {
-  var orbitRadius = 20
-  var orbitRadiusmoon = 30
-  var orbitRadiusSun = 40
 
-  var orbitsat
-  var orbitmoon
-  var orbitcloud
-  var orbitsun
-  orbitsat = Date.now() * 0.0001
-  orbitmoon = Date.now() * 0.0001
-  orbitcloud = Date.now() * 0.0001
-  orbitsun = Date.now() * 0.001
+  var orbitRadius = 20; 
+  var orbitRadiusmoon = 30;
+  var orbitRadiusSun = 40;
 
-  if (globalEarth && globalSat && globalCloud && globalCloud2 && globalMoon) {
-    // Earth
-    globalEarth.rotation.z += 0.001
+  var orbitsat;
+  var orbitmoon;
+  var orbitcloud;
+  var orbitsun;
+  orbitsat = Date.now() * 0.0001;
+  orbitmoon = Date.now() * 0.0001;
+  orbitcloud = Date.now() * 0.0001;
+  orbitsun = Date.now() * 0.001;
 
-    // Moon
-    globalMoon.rotation.z += 0.01
-    globalMoon.rotation.x += 0.01
-    globalMoon.position.set(1.2 * Math.cos(orbitmoon) * orbitRadiusmoon, 0, 1.2 * Math.sin(orbitmoon) * orbitRadiusmoon)
+ if(globalEarth && globalSat && globalCloud && globalCloud2 && globalMoon){
 
-    // Sat
-    globalSat.rotation.x += 0.01
-    globalSat.position.set(
-      Math.cos(orbitsat * 1.3) * orbitRadius,
-      Math.sin(orbitsat * 1.3) * orbitRadius,
-      orbitRadius,
-      Math.sin(orbitsat * 1.3) * orbitRadius
-    )
+  // Earth 
+  globalEarth.rotation.z += 0.001; 
 
-    // Cloud
-    globalCloud.rotation.z *= 0.0012
-    globalCloud.position.set(
-      0.7 * Math.cos(orbitcloud * 1.1) * orbitRadius,
-      0.7 * Math.sin(orbitcloud * 1.1) * orbitRadius,
-      0
-    )
+  // Moon
+  globalMoon.rotation.z += 0.01;
+  globalMoon.rotation.x += 0.01;
+  globalMoon.position.set(1.2*Math.cos(orbitmoon) * orbitRadiusmoon, 0 , 1.2*Math.sin(orbitmoon) * orbitRadiusmoon); 
 
-    // Cloud 2
-    globalCloud2.position.set(Math.cos(orbitcloud * 1.15) * orbitRadius, 0, Math.sin(orbitcloud * 1.15) * orbitRadius)
+  // Sat
+  globalSat.rotation.x += 0.01; 
+  globalSat.position.set(Math.cos(orbitsat*1.3) * orbitRadius, Math.sin(orbitsat*1.3) * orbitRadius , orbitRadius, Math.sin(orbitsat*1.3) * orbitRadius );
 
-    // Sun rotation & Light rotation
-    globalSun.position.set(Math.cos(orbitsun) * orbitRadiusSun, 0, Math.sin(orbitsun) * orbitRadiusSun)
+  // Cloud
+  globalCloud.rotation.z *= 0.0012;
+  globalCloud.position.set(0.7*Math.cos(orbitcloud*1.1) * orbitRadius, 0.7*Math.sin(orbitcloud*1.1) * orbitRadius , 0);
 
-    pointLight.position.set(Math.cos(orbitsun) * orbitRadiusSun, 0, Math.sin(orbitsun) * orbitRadiusSun)
-  }
+  // Cloud 2
+  globalCloud2.position.set(Math.cos(orbitcloud*1.15) * orbitRadius, 0, Math.sin(orbitcloud*1.15) * orbitRadius);
+
+  // Sun rotation & Light rotation
+  globalSun.position.set(Math.cos(orbitsun) * orbitRadiusSun, 0 , Math.sin(orbitsun) * orbitRadiusSun); 
+  
+  pointLight.position.set(Math.cos(orbitsun) * orbitRadiusSun, 0 , Math.sin(orbitsun) * orbitRadiusSun); 
+
+}
 
   // Render
   renderer.render(scene, camera)
 
   // Call tick again on the next frame
   window.requestAnimationFrame(tick)
+
 }
 
 tick()
